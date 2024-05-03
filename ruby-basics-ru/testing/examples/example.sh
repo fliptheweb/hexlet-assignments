f993c0be7795481778c2ccea6e3e5185dc842859
Artur Kornakov <itisshortemailaddress@gmail.com>
2024-05-03 11:51:36 +0300

    test(): stack

--- ruby-basics-ru/testing/examples/example.sh
+++ ruby-basics-ru/testing/examples/example.sh
@@ -1,15 +1,15 @@
 ### Запуск тестов
 ## Обычный запуск тестов
-ruby example_test.rb
+ruby example.rb
 
 ## Запуск с детальным логирование выполнения
-ruby ex=begin  =endample_test.rb -v
+ruby example.rb -v
 
 ## Запуск с паттерном названия метода
-ruby example_test.rb -n test_one
+ruby example.rb -n test_one
 
 ## Запуск с исключающим паттерном названия метода
-ruby example_test.rb -e test_one
+ruby example.rb -e test_one
 
 ## Запуск с детерминированным сидом для псевдослучайного генератора
-ruby example_test.rb -s 10000
+ruby example.rb -s 10000
--- ruby-basics-ru/testing/test/stack_test.rb
+++ ruby-basics-ru/testing/test/stack_test.rb
@@ -5,7 +5,31 @@ require_relative '../lib/stack'
 
 class StackTest < Minitest::Test
   # BEGIN
+  def test_item_add
+    stack1 = Stack.new([])
+    stack2 = Stack.new([1])
+    stack1.push!(1)
+    assert { stack1.to_a == stack2.to_a }
+  end
 
+  def test_item_remove
+    stack1 = Stack.new([1, 2, 3])
+    stack2 = Stack.new([1, 2])
+    stack1.pop!
+    assert { stack1.to_a == stack2.to_a }
+  end
+
+  def test_clear
+    stack = Stack.new([1, 2, 3])
+    assert { stack.clear!.empty? == true }
+  end
+
+  def test_is_empty
+    stack1 = Stack.new([])
+    stack2 = Stack.new([1])
+    assert { stack1.empty? == true }
+    assert { stack2.empty? == false }
+  end
   # END
 end
 
