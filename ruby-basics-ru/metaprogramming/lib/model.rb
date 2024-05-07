10cf1cc157362ec5d0272c9c7317499e9cb90dcf
Artur Kornakov <itisshortemailaddress@gmail.com>
2024-05-07 13:48:39 +0300

    feat(): metaprogramming complete

---
+++ ruby-basics-ru/metaprogramming/lib/model.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+# https://github.com/solnic/virtus/blob/master/lib/virtus.rb
+# BEGIN
+module Model
+  # How we can create class methods in different way?
+  def self.included(base)
+    base.extend ClassMethods
+  end
+
+  module ClassMethods
+    attr_reader :scheme
+
+    def attribute name, options = {}
+      @scheme ||= {}
+      @scheme[name] = options
+
+      define_method name do
+        value = instance_variable_get "@#{name}"
+      end
+
+      define_method "#{name}=" do |value|
+        instance_variable_set "@#{name}", cast(value, options[:type])
+      end
+    end
+  end
+
+  def attributes
+    self.class.scheme.each_with_object({}) do |(name), acc|
+      acc[name] = send name
+    end
+  end
+
+  def initialize attributes = {}
+    self.class.scheme.each do |name, options|
+      value = attributes.key?(name) ? attributes[name] : options[:default]
+      instance_variable_set "@#{name}", cast(value, options[:type])
+    end
+  end
+
+  private
+
+  def cast value, type
+    return value if type.nil? || value.nil?
+
+    case type
+      when :integer
+        value.to_i
+      when :string
+        value.to_s
+      when :boolean
+        value.to_s === 'true'
+      when :datetime
+        DateTime.parse(value)
+      else
+        value
+    end
+  end
+end
+# END
