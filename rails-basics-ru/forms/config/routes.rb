9675b1d68dca62029df5ad1cd59508039aff097f
Artur Kornakov <itisshortemailaddress@gmail.com>
2024-07-18 18:02:34 +0300

    feat(): complete templates

--- rails-basics-ru/forms/config/routes.rb
+++ rails-basics-ru/forms/config/routes.rb
@@ -3,7 +3,12 @@
 Rails.application.routes.draw do
   root 'home#index'
   # BEGIN
-  
+  get 'posts/index'
+  get 'posts/show'
+  get 'posts/edit'
+
+  resources :posts
+
   # END
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
